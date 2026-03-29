fde77bc37d5d03b23e68a77bc33ae0e8973f62f2
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2026-03-28 21:17:50 -0500

    added option screen, improved graphics, reorganized scripts and scenes

--- kana-warrior/scripts/game_over.gd
+++ kana-warrior/scripts/game_over.gd
@@ -11,13 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_pressed() -> void:
+func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/selectLevel.tscn")
-
-
-func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/OptionScreen.tscn")
-
-
-func _on_exit_pressed() -> void:
-	get_tree().quit()
